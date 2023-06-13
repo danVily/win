abbc8fc0d9d2e972c931a2b02a7af193a4d72f79
danVily <feafas499@gmail.com>
2023-06-13 20:39:40 +0300

    Initial commit

---
+++ generate_fibonacci_sequence.py
@@ -0,0 +1,12 @@
+generate_fibonacci_sequence
+def generate_fibonacci_sequence(n):
+    sequence = [0, 1]  
+    for i in range(2, n):
+        next_number = sequence[i-1] + sequence[i-2]
+        sequence.append(next_number)
+    return sequence
+
+    
+fibonacci_sequence = generate_fibonacci_sequence(40)
+for number in fibonacci_sequence:
+    print(number)
